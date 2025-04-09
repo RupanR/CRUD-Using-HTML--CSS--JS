aedc0901518f8e0f6fe0ba2c7e87dc71f5736325
RupanR <rupanmentor@gmail.com>
2025-04-09 21:02:57 +0530

    create,read completed

---
+++ script.js
@@ -0,0 +1,73 @@
+//! Getting all elements from html
+const form = document.getElementById("form");
+const textInput = document.getElementById("textInput");
+const dateInput = document.getElementById("dateInput");
+const textarea = document.getElementById("textarea");
+const msg = document.getElementById("msg");
+const tasks = document.getElementById("tasks");
+const add = document.getElementById("add");
+
+//! Form validation
+
+const formValidation = () => {
+  if (
+    textInput.value === "" ||
+    dateInput.value === "" ||
+    textarea.value === ""
+  ) {
+    msg.innerHTML = "Input Fields Cannot Be Empty 😞";
+    //console.log("data not found");
+  } else {
+    msg.innerHTML = "";
+    //console.log("data found");
+    getData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
+};
+
+
+//! Submit logic
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    formValidation();
+})
+
+//! getting details from the form input and storing it in data in array of objects.
+
+let data = [{}]
+
+const getData = ()=>{
+    data.push({
+        text: textInput.value,
+        date: dateInput.value,
+        task: textarea.value
+    })
+    
+    //to save the data to the local storage
+    localStorage.setItem("data", JSON.stringify(data));
+  
+    createTask();
+}
+
+//! Create function used to get the data from local storage and display it in my todo's
+
+const createTask = () =>{
+    tasks.innerHTML =""
+    data.map((ele,y)=>{
+        return(
+            tasks.innerHTML += `
+            <div id=${y}>
+            <span class="fw-bolder">${ele.text}</span>
+            <span class="fw-bolder">${ele.date}</span>
+            <p class="fw-bold">${ele.task} </p>
+            </div>
+            ` 
+        )
+    })
+
+}
